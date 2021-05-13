5386ac1e7ad52aec4e99acf38d7ab8d753585b84
flavioro <flavioro@gmail.com>
2021-05-12 23:31:31 -0300

    update UserSchema

--- src/schemas/UserSchema.ts
+++ src/schemas/UserSchema.ts
@@ -1,14 +1,20 @@
 import { Schema, model, Document } from 'mongoose'
 
-interface UserInterface extends Document{
-    name:string;
-    email:string;
-    password: string;
+interface UserInterface extends Document {
+  name: string;
+  email: string;
+  password: string;
+  account: Number;
+  dtTest: Date;
+  dtLicence: Date;
 }
 const UserSchema = new Schema({
   name: String,
   email: String,
-  password: String
+  password: String,
+  account: Number,
+  dtTest: Date,
+  dtLicence: Date,
 })
 
 export default model<UserInterface>('user', UserSchema)
